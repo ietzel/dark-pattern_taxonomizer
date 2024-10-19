315d414ae80b949ae5f1fc08b202981aee07673a
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-19 15:43:55 -0400

    Update dp_ptq.lisp

--- dp_ptq.lisp
+++ dp_ptq.lisp
@@ -3,7 +3,7 @@
 (format t "Obstructed: Makes it difficult for users to interact.")
 (format t "Sneaked: Hides or delays useful information.")
 (format t "Interface-interfered: Manipulates the user interface to emphasize certain aspects.")
-(format t " Forced-actioned: Requires users to perform an action to access functionality.")
+(format t "Forced-actioned: Requires users to perform an action to access functionality.")
 (format t "Overloaded: Presents a large amount of information to prompt users to share more data.")
 (format t "Stirred: Uses visual nudges or appeals to users' emotions to affect their choices.")
 (format t "Hindered: Makes it difficult or impossible for users to manage their data or learn about their privacy options.")
@@ -11,4 +11,3 @@
 (format t "Left in the dark: Hides data protection control tools or information, or leaves users unsure of how their data is processed.")
 (format t "De facto forced: Forces users to take an action to perform another action 
 (format t "Obstacled: Dissuades users from taking certain actions using obstacles 
-
