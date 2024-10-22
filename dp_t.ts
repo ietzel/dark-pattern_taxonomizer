ace09fa02cfcf0ca755b17656872afdc49d78ec6
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-22 10:32:01 -0400

    Update and rename dp_t.pli to dp_t.ts

---
+++ dp_t.ts
@@ -0,0 +1,18 @@
+console.log("The dark patterns you aren't wary of are not your dark pattern personality type.");
+console.log("Nagged: Redirects users to expected functionality.");
+console.log("Obstructed: Makes it difficult for users to interact.");
+console.log("Sneaked: Hides or delays useful information.");
+console.log("Interface-interfered: Manipulates the user interface to emphasize certain aspects.");
+console.log("Forced-actioned: Requires users to perform an action to access functionality.");
+console.log("Overloaded: Presents a large amount of information to prompt users to share more data.");
+console.log("Stirred: Uses visual nudges or appeals to users' emotions to affect their choices.");
+console.log("Hindered: Makes it difficult or impossible for users to manage their data or learn about their privacy options.");
+console.log("Fickled: Inconsistent or unclear design that makes it difficult for users to understand the purpose of data processing or navigate data protection control tools.");
+console.log("Left in the dark: Hides data protection control tools or information, or leaves users unsure of how their data is processed.");
+console.log("De facto forced: Forces users to take an action to perform another action."); 
+console.log("Obstacled: Dissuades users from taking certain actions using obstacles.");
+console.log("others include:.");
+console.log("7 chakra targeting.");
+console.log("qualia that stiffen spines.");
+console.log("black & green euler spirals.");
+console.log("x^2 + 4 geometry, or otherwise followings of the numeric sequence.");
